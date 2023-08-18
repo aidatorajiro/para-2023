53d89476991e07a22be1caa07b247d4492e54dd9
aidatorajiro <kawarusosu@zoho.com>
2023-08-18 12:52:04 +0900

    クオータニオン係数再調整

--- vr-web/src/MyApp.tsx
+++ vr-web/src/MyApp.tsx
@@ -41,9 +41,12 @@ class MyApp extends React.Component<MyProps, MyState> {
       this.websocket.addEventListener('message', (e) => {
         let d: CommData[] = JSON.parse(e.data)
         let latest = d[0]
-        obj?.setRotationFromQuaternion(new THREE.Quaternion(latest.quaternion[1], -latest.quaternion[0], -latest.quaternion[2], latest.quaternion[3]))
 
-        obj?.position.add(new THREE.Vector3(...latest.linear_acceleration))
+        obj?.setRotationFromQuaternion(new THREE.Quaternion(-latest.quaternion[1], latest.quaternion[0], latest.quaternion[2], -latest.quaternion[3]))
+
+        obj?.position.add(new THREE.Vector3(...latest.linear_acceleration).divideScalar(10))
+
+        console.log(latest.linear_acceleration)
       })
     }
   
