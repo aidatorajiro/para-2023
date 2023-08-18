c36e18544ac0a6ace24e8cb866c56be1e294cb17
aidatorajiro <kawarusosu@zoho.com>
2023-08-18 16:31:46 +0900

    hnnn

--- rpi-root/main.py
+++ rpi-root/main.py
@@ -7,6 +7,7 @@ import os
 import wifi
 import socketpool
 import json
+from struct import *
 
 time.sleep(3)
 
@@ -31,22 +32,44 @@ sock = try_socket_forever()
 
 
 
-i2c = bitbangio.I2C(board.GP1, board.GP0, frequency=5000, timeout=100000)
+i2c = bitbangio.I2C(board.GP1, board.GP0, frequency=400000, timeout=100000)
 sensor = adafruit_bno055.BNO055_I2C(i2c)
 
-data = {}
+data = []
+
+td = None
+
+t_all = 0
+
+pos_x = 0
+pos_y = 0
+pos_z = 0
 
 while True:
+    t1 = time.monotonic_ns()
+    
+    if td is not None:
+        t_all += td
+
     try:
-        data["quaternion"] = sensor.quaternion
-        data["linear_acceleration"] = sensor.linear_acceleration
-        data["calibration_status"] = sensor.calibration_status
-        data["time"] = time.monotonic_ns()
+        data.append([sensor.quaternion,
+                     sensor.linear_acceleration,
+                     sensor.calibration_status])
     except Exception as e:
         print("err")
     
-    try:
-        sock.sendall(json.dumps(data) + "\n")
-    except OSError:
-        sock = try_socket_forever()
-        sock.sendall(json.dumps(data) + "\n")
+    if t_all > 100000000:
+        #senddata = b""
+        #for x in data:
+        #    senddata += pack("dddddddbbbbQ", *(x[0] + x[1] + x[2] + (x[3],)))
+        #try:
+        #    sock.sendall(senddata + "\n")
+        #except OSError:
+        #    sock = try_socket_forever()
+        #    sock.sendall(senddata + "\n")
+        print(len(data))
+        data = []
+        t_all = 0
+    
+    t2 = time.monotonic_ns()
+    td = t2 - t1
