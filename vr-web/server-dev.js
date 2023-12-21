bf32778b25d34b9809ceb84658b92b54720838ae
aidatorajiro <kawarusosu@zoho.com>
2023-12-21 16:24:03 +0900

    add upload button

--- vr-web/server-dev.js
+++ vr-web/server-dev.js
@@ -1,18 +1,33 @@
 //expressモジュールの読み込み
 const express = require('express')
+const expressFormData = require('express-form-data');
 const path = require('path')
+
+const PORTNUM = 7677
 
 //expressのインスタンス化
 const app = express()
 
-app.use(express.json())
-
-
-app.listen(7677, () => {
+app.listen(PORTNUM, () => {
   console.log("サーバー起動中");
 });
+
+app.use('/api/log', express.json())
 
 app.post('/api/log', function (req, res) {
   console.log(req.body)
   res.json({ok: 'ok'})
 })
+
+const fs = require('fs')
+
+app.use('/api/upload_glb', expressFormData.parse({uploadDir: __dirname + '/uploads', autoClean: true}));
+
+app.post('/api/upload_glb', function (req, res) {
+  if (req.files.glbfile) {
+    fs.copyFileSync(req.files.glbfile.path, __dirname + '/dist/model.glb')
+  }
+  res.json({ok: 'ok'})
+})
+
+app.use('/api/interface', express.static(__dirname + '/api_interface'))
